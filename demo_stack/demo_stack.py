8b7188416ad73f7d26a8e7e45b261e8b600694d3
alanjosue97 <perezaln.09@gmail.com>
2023-08-15 16:58:14 -0600

    bucket creation was add

--- demo_stack/demo_stack.py
+++ demo_stack/demo_stack.py
@@ -16,45 +16,5 @@ class DemoAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
          
-        vpc = ec2.Vpc(
-            self,
-            "EcsClusterVpc",
-            max_azs=2,
-        )
-
-        cluster = ecs.Cluster(
-            self,
-            "EcsCluster",
-            vpc=vpc
-        )
-        ecs_task_role = iam.Role(
-            self,
-            "EcsTaskRole",
-            assumed_by = iam.ServicePrincipal("ecs-task.amazon.com"),
-            description="Grant access to multiple AWS services"
-        )
-
-        ecr_repository = ecr.Repository.from_repository_name(
-            self,
-            "EcrRepository",
-            "demo-api-salon",
-        )
-
-        ecr_image = ecs.ContainerImage.from_ecr_repository(
-            ecr_repository,
-            "latest",
-        )
-        fargate_cluster = ecs_patterns.ApplicationLoadBalancedFargateService(
-            self,
-            "EcsFargateService",
-            cluster=cluster,
-            memory_limit_mib=1024,
-            cpu=512,
-            desired_count=1,
-            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-                image=ecr_image,
-                task_role=ecs_task_role,
-                
-            ),
-        )
+        bucket =s3.Bucket(self, "Demo-api")
       
