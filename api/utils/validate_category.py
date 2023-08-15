1a13cf2941f85040fe93d36d6bc300426876e67f
alanjosue97 <perezaln.09@gmail.com>
2023-08-15 08:20:18 -0600

    README modificated

---
+++ api/utils/validate_category.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+from fastapi import Header
+from utils.database import get_db
+
+async def validate_category(category: Annotated[str, Header()]):
+    database = await get_db()
+
+    category_exists = await database.categories.find_one({"category": category})
+    print(category_exists)
+    return True
