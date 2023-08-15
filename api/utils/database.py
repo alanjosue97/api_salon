dff3af49fc7ba68738b4ba81a723c07309b7c740
alanjosue97 <perezaln.09@gmail.com>
2023-08-14 19:01:44 -0600

    dockerfile DONE

--- api/utils/database.py
+++ api/utils/database.py
@@ -1,5 +1,10 @@
+import os
+ 
 from motor.motor_asyncio import AsyncIOMotorClient
 
+MONGO_INITDB_ROOT_USERNAME=os.getenv("MONGO_INITDB_ROOT_USERNAME")
+MONGO_INITDB_ROOT_PASSWORD=os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+
 async def get_db():
-    client = AsyncIOMotorClient("mongodb+srv://salon:Salon123@salon.ituskvf.mongodb.net/?retryWrites=true&w=majority")
+    client = AsyncIOMotorClient(f"mongodb+srv://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@salon.ituskvf.mongodb.net/?retryWrites=true&w=majority")
     return client.control
